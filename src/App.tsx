9ef5a15dfaffce86a2974bd31567d26abad6b3ae
Ekaterina Gnedysheva <gnedyshevaeg@gmail.com>
2026-03-07 17:52:48 +0100

    red green

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
 import Accordion from "./Accordion.tsx";
 import {Rating} from "./Rating.tsx";
+import {OnOff} from "./components/OnOff.tsx";
 
 function App() {
 
 
   return (
     <>
-      <Rating value={2}/>
-        <Rating value={4}/>
-        <Accordion title={'Menu'} collapsed={true}/>
-      <Accordion title={'Users'} collapsed={false}/>
+      <OnOff />
+        <OnOff />
+      {/*<Rating value={2}/>*/}
+      {/*  <Rating value={4}/>*/}
+      {/*  <Accordion title={'Menu'} collapsed={true}/>*/}
+      {/*<Accordion title={'Users'} collapsed={false}/>*/}
+
     </>
   )
 }
